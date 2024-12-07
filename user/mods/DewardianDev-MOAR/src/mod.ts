3b7854d3fdd2d2487ca3d991cf70958fe24deee3
IAMBUDE <93278696+IAMBUDE@users.noreply.github.com>
2024-12-07 19:39:38 +0100

    Automated Commit
    Check change list for updates.

---
+++ user/mods/DewardianDev-MOAR/src/mod.ts
@@ -0,0 +1,30 @@
+import { DependencyContainer } from "tsyringe";
+import { IPostSptLoadMod } from "@spt/models/external/IPostSptLoadMod";
+import { IPreSptLoadMod } from "@spt/models/external/IPreSptLoadMod";
+import { StaticRouterModService } from "@spt/services/mod/staticRouter/StaticRouterModService";
+import { enableBotSpawning } from "../config/config.json";
+import { buildWaves } from "./Spawning/Spawning";
+import config from "../config/config.json";
+import { globalValues } from "./GlobalValues";
+import { ILogger } from "@spt/models/spt/utils/ILogger";
+import { setupRoutes } from "./Routes/routes";
+import { adjustSpawnPoints } from "./SpawnPoints/SpawnPoints";
+
+class Moar implements IPostSptLoadMod, IPreSptLoadMod {
+  postSptLoad(container: DependencyContainer): void {
+    if (enableBotSpawning) {
+      globalValues.baseConfig = config;
+      globalValues.overrideConfig = {};
+      // adjustSpawnPoints(container);
+      const logger = container.resolve<ILogger>("WinstonLogger");
+      logger.info("\nMOAR: Starting up, may the bots ever be in your favour!");
+      buildWaves(container);
+    }
+  }
+
+  preSptLoad(container: DependencyContainer): void {
+    if (enableBotSpawning) setupRoutes(container);
+  }
+}
+
+module.exports = { mod: new Moar() };
